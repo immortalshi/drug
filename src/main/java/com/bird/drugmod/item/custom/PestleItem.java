c49f995fd7588edf1ac0853126b3c329a912aafb
immortalshi <1446876461@qq.com>
2025-02-19 00:17:45 +0800

    配置mapping失败

---
+++ src/main/java/com/bird/drugmod/item/custom/PestleItem.java
@@ -0,0 +1,12 @@
+package com.bird.drugmod.item.custom;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Vanishable;
+
+public class PestleItem extends SwordItem implements Vanishable {
+    public PestleItem(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
+        super(p_43269_, p_43270_, p_43271_, p_43272_);
+    }
+}
