6be0f2bfe7ad512db9d87decd5f7f12512baf333
immortalshi <1446876461@qq.com>
2025-02-25 21:46:23 +0800

    添加AI暂停机制

--- src/main/java/com/bird/drugmod/item/custom/PestleItem.java
+++ src/main/java/com/bird/drugmod/item/custom/PestleItem.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
 
 public class PestleItem extends SwordItem implements Vanishable {
 
@@ -26,9 +27,12 @@ public class PestleItem extends SwordItem implements Vanishable {
 
         Mob mob = (Mob)pTarget;
         mob.setNoAi(true);
-        
 
+        Block block = pTarget.getBlockStateOn().getBlock();
 
+        mob.getCommandSenderWorld().scheduleTick(block, 100, () -> {
+            mob.setNoAi(false);  // 5秒后恢复AI
+        });
         return true;
     }
 
